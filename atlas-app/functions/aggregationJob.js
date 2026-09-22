e5aa579b93c6b93e0dd617fc8582f4b64da02d70
Manuel Pandrea <manuel.pandrea@gmail.com>
2026-09-22 21:27:17 +0300

    comment db connection

--- atlas-app/functions/aggregationJob.js
+++ atlas-app/functions/aggregationJob.js
@@ -1,10 +1,11 @@
 exports = async function() {
+    /*
     const db = context.services
         .get("mongodb-atlas")
         .db("market_data");
 
     const bars1m = db.collection("bars_1m");
-
+*/
     console.log("Daily aggregation started");
 
     // aggregation code here
